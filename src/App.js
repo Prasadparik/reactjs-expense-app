2547bc22c58052eac22c4ab20602029d2070a76c
Prasad Parik <56125377+Prasadparik@users.noreply.github.com>
2023-12-23 12:46:07 +0530

    expensefilter added

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={data} />
+      <Expenses items={expenses} />
     </div>
   );
 }
