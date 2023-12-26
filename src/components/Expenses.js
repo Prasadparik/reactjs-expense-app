9e99fef6e23a846ca7175db2718dd9d19331d810
Prasad Parik <56125377+Prasadparik@users.noreply.github.com>
2023-12-26 13:03:34 +0530

    Conditional Rendering

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -5,27 +5,45 @@ import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
-  const [filteredYear, setFilteredYear] = useState("2020");
+  const [filteredYear, setFilteredYear] = useState("2021");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+  const getYearFromDate = (date) => date.getFullYear();
+  let filterData = props.items.filter(
+    (elem) => getYearFromDate(elem.date) == filteredYear
+  );
+
+  let expensesContent = <p style={{ color: "#fff" }}>No expenses found</p>;
+
+  if (filterData.length > 0) {
+    expensesContent = filterData.map((elem) => {
+      return (
+        <ExpenseItem
+          key={elem.id}
+          title={elem.title}
+          amount={elem.amount}
+          date={elem.date}
+        />
+      );
+    });
+  }
+  console.log("TEST :: ", filterData);
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {props.items.map((elem) => {
-        return (
-          <ExpenseItem
-            key={elem.id}
-            title={elem.title}
-            amount={elem.amount}
-            date={elem.date}
-          />
-        );
-      })}
+      {expensesContent}
+      {filterData.length === 1 ? (
+        <p style={{ color: "#fff" }}>
+          Only single Expense here. Please add more...
+        </p>
+      ) : (
+        <></>
+      )}
     </Card>
   );
 }
